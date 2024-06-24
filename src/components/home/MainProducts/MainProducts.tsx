2a98b3a1bdbe6e820a0ab4bf11bb0408f3818a05
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-06-24 13:44:24 -0600

    Getting products from Shopify API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,10 +1,23 @@
-"use client";
+const getProducts = async () => {
+  const res = await fetch(
+    `https://${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-04/products.json`,
+    {
+      method: 'GET',
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
+      }),
+    }
+  );
+  const data = await res.json();
+  return data;
+};
 
-export const MainProducts = () => {
-  console.log("env:", process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
+export const MainProducts = async () => {
+  const products = await getProducts();
+  console.log('products:', products);
   return (
     <section>
       <h1>Main Products</h1>
     </section>
-  )
-}
+  );
+};
