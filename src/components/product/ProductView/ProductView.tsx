ee37f8d4f7cbb4f2189249e46786c70dd4528e10
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-07-05 21:57:06 -0600

    Sanitize HTML

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,20 +1,14 @@
-'use client';
 import Image from 'next/image';
 import { ProductViewItemsOrder } from './ProductViewItemsOrder';
 import styles from './ProductView.module.sass';
 import { ProductType } from '../../../../types';
-import { useRouter } from 'next/navigation';
+import { SanitizeHTML } from 'app/components/shared/SanitizeHTML/SanitizeHTML';
 interface ProductViewProps {
   product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
-  const router = useRouter();
 
-  if (!product) {
-    router.push('/');
-    return null;
-  }
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
@@ -30,9 +24,9 @@ export const ProductView = ({ product }: ProductViewProps) => {
       <section className={styles.ProductView__info}>
         <h1 className={styles.ProductView__info__title}>{product.title}</h1>
         <p className={styles.ProductView__info__category}>{product.tags}</p>
-        <p className={styles.ProductView__info__description}>
+        <SanitizeHTML tag="p" className={styles.ProductView__info__description}>
           {product.description}
-        </p>
+        </SanitizeHTML>
         <span className={styles.ProductView__info__price}>
           $ {product.price}
         </span>
---
+++ src/components/shared/SanitizeHTML/SanitizeHTML.tsx
@@ -0,0 +1,16 @@
+import { createElement } from 'react';
+import { HTMLAttributes } from 'react';
+import sanitizeHtml from 'sanitize-html';
+
+type SanitizeHTMLProps = {
+  children: string;
+  tag: string;
+} & HTMLAttributes<HTMLElement>;
+
+export const SanitizeHTML = ({ tag, children, ...rest }: SanitizeHTMLProps) => {
+  const sanitizedHtml = sanitizeHtml(children, {
+    allowedTags: ['b', 'i', 'em', 'strong'],
+  });
+
+  return createElement(tag, { ...rest }, sanitizedHtml);
+};
