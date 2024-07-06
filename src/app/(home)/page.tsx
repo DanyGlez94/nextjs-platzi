d8ebd40953e5814e410bef16fd7becb219e73505
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-07-05 22:22:41 -0600

    Improving SEO of static and dynamic pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "🚀 Future world",
+  description: "Welcome to the future world, an ecommerce from other century!",
+  keywords: ["future", "world", "ecommerce", "century"],
+};
 
 export default function Home() {
-
   return (
     <main>
       <MainProducts />
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,20 +1,48 @@
 import { ProductView } from 'app/components/product/ProductView';
 import { getProducts } from 'app/services/shopify/products';
 import { redirect } from 'next/navigation';
+import sanitizeHtml from 'sanitize-html';
 interface ProductPageProps {
   searchParams: {
     id: string;
   };
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps ) {
+  const id = searchParams.id;
+  const products = await getProducts(id);
+  const product = products[0];
+
+  const sanitizedDescription = sanitizeHtml(product.description, {
+    allowedTags: ['b', 'i', 'em', 'strong'],
+  });
+
+  return {
+    title: product.title,
+    description: sanitizedDescription,
+    keywords: product.tags,
+    openGraph: {
+      images: [
+        {
+          url: product.image,
+          width: 800,
+          height: 600,
+          alt: product.title
+        }
+      ]
+    }
+  };
+}
+
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
   const product = products[0];
 
-  // if (!id) {
-  //   redirect('/');
-  // }
+  if (!id) {
+    redirect('/');
+  }
   
   return (
     <div>
